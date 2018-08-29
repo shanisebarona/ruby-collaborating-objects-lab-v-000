4abce58ea1f4036ee83adb7cb6e2f36007dc2de4
Shanise Barona <shanisebarona@gmail.com>
2018-08-29 22:57:28 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+#create the artist or finging the instance of the aritst 
+require 'pry'
+class Artist
+  attr_accessor :name 
+  
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def save
+   @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.find_or_create_by_name(name)
+    @@all.find { |artist| return artist if artist.name == name}
+    new_artist = Artist.new(name)
+    new_artist.save
+    new_artist
+  end
+  
+  def print_songs
+    binding.pry
+    puts @songs.name
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+  
+end
